dffb7fc9b1d4db03d9ee4a6039cde0d808d1c3e5
Jared Hendrickson <jhendrickson@securitymetrics.com>
2021-02-15 22:54:08 -0700

    Change some install and uninstall tasks

--- pfSense-pkg-saml2-auth/install.sh
+++ pfSense-pkg-saml2-auth/install.sh
@@ -7,7 +7,7 @@ cp /etc/inc/authgui.inc /etc/inc/authgui.inc.original
 cp /etc/inc/priv.inc /etc/inc/priv.inc.original
 
 # Copy package contents to PHP inc path
-cp -r "$BASEDIR/pfSense-pkg-saml2-auth/files/etc/inc/saml2_auth" "/etc/inc/"
+cp -r "$BASEDIR/files/etc/inc/saml2_auth" "/etc/inc/"
 
 # Copy file overrides to their respective destintations
 cp /etc/inc/saml2_auth/overrides/auth.inc /etc/inc/auth.inc
@@ -15,6 +15,6 @@ cp /etc/inc/saml2_auth/overrides/authgui.inc /etc/inc/authgui.inc
 cp /etc/inc/saml2_auth/overrides/priv.inc /etc/inc/priv.inc
 
 # Copy static content
-cp -r "$BASEDIR/pfSense-pkg-saml2-auth/files/usr/local/www/saml2_auth" "/usr/local/www/saml2_auth"
-cp "$BASEDIR/pfSense-pkg-saml2-auth/files/usr/local/www/js/saml2_auth.js" "/usr/local/www/js/saml2_auth.js"
-cp "$BASEDIR/pfSense-pkg-saml2-auth/files/usr/local/www/css/saml2_auth.css" "/usr/local/www/css/saml2_auth.css"
+cp -r "$BASEDIR/files/usr/local/www/saml2_auth" "/usr/local/www/saml2_auth"
+cp "$BASEDIR/files/usr/local/www/js/saml2_auth.js" "/usr/local/www/js/saml2_auth.js"
+cp "$BASEDIR/files/usr/local/www/css/saml2_auth.css" "/usr/local/www/css/saml2_auth.css"
