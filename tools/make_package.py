85aab6f4eaf27be7b58912c5aa9e7459109ebc81
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-11-08 20:09:14 -0700

    build: refactor make_package.py for composer
    updates make_package.py to install and include files via composer

--- tools/make_package.py
+++ tools/make_package.py
@@ -12,36 +12,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Script that is used to build the pfSense-pkg-saml2-auth package on FreeBSD."""
 
+import argparse
+import getpass
 import os
-import sys
 import pathlib
-import jinja2
 import platform
 import subprocess
-import getpass
-import argparse
+import sys
+import jinja2
+
+# Constants
+PKG_NAME = "pfSense-pkg-saml2-auth"
+REPO_NAME = "pfsense-saml2-auth"
+
 
 class MakePackage:
+    """Class that groups together variables and methods required to build the pfSense-pkg-saml2-auth FreeBSD package."""
     def __init__(self):
         self.__start_argparse__()
         self.port_version = ".".join(self.args.tag.split(".")[0:2])
         self.port_revision = self.args.tag.split(".")[2]
 
         # Run tasks for build mode
-        if self.args.build:
+        if self.args.host:
             self.build_on_remote_host()
         else:
             self.generate_makefile()
 
-    # Custom filter for Jinja2 to determine the directory of a given file path
     def dirname(self, path):
+        """Custom filter for Jinja2 to determine the directory of a given file path"""
         return os.path.dirname(path)
 
     def generate_makefile(self):
+        """Generates the Makefile for this build."""
+        # Many variables are needed for various files and filepaths
+        # pylint: disable=too-many-locals
+
         # Set filepath and file variables
         root_dir = pathlib.Path(__file__).absolute().parent.parent
-        pkg_dir = root_dir.joinpath("pfSense-pkg-saml2-auth")
+        pkg_dir = root_dir.joinpath(PKG_NAME)
         template_dir = root_dir.joinpath("tools").joinpath("templates")
         files_dir = pkg_dir.joinpath("files")
         file_paths = {"dir": [], "file": [], "port_version": self.port_version, "port_revision": self.port_revision}
@@ -54,71 +65,70 @@ class MakePackage:
         makefile_template = j2_env.get_template("Makefile.j2")
 
         # Loop through each of our files and directories and store them for Jinja2 to render
-        for root, dirs, files in os.walk(files_dir, topdown=True):
+        for root, directories, files in os.walk(files_dir, topdown=True):
             root = pathlib.Path(str(root).replace(str(files_dir), ""))
-            for dir in dirs:
-                if dir not in excluded_files:
-                    file_paths["dir"].append(str(root.joinpath(dir)))
+            for directory in directories:
+                if directory not in excluded_files:
+                    file_paths["dir"].append(str(root.joinpath(directory)))
             for file in files:
                 if file not in excluded_files:
                     file_paths["file"].append(str(root.joinpath(file)))
 
         # Generate pkg-plist file
-        with open(pkg_dir.joinpath("pkg-plist"), "w") as pw:
-            pw.write(plist_template.render(files=file_paths))
+        with open(pkg_dir.joinpath("pkg-plist"), "w", encoding="utf-8") as pkg_plist:
+            pkg_plist.write(plist_template.render(files=file_paths))
         # Generate Makefile file
-        with open(pkg_dir.joinpath("Makefile"), "w") as mw:
-            mw.write(makefile_template.render(files=file_paths).replace("   ", "\t"))
+        with open(pkg_dir.joinpath("Makefile"), "w", encoding="utf-8") as makefile:
+            makefile.write(makefile_template.render(files=file_paths).replace("   ", "\t"))
 
         self.build_package(pkg_dir)
 
     def run_ssh_cmd(self, cmd):
-        ssh_cmd = "ssh {u}@{h} '{c}'".format(u=self.args.username, h=self.args.host, c=cmd)
+        """Formats the SSH command to use when building on remote hosts."""
+        ssh_cmd = f"ssh {self.args.username}@{self.args.host} '{cmd}'"
         return subprocess.call(ssh_cmd, shell=True)
 
     def run_scp_cmd(self, src, dst, recurse=False):
-        scp_cmd = "scp {r} {s} {d}".format(r="-r" if recurse else "", s=src, d=dst)
+        """Formats the SCP command to use when copying over the built package."""
+        scp_cmd = f"scp {'-r' if recurse else ''} {src} {dst}"
         return subprocess.call(scp_cmd, shell=True)
 
     def build_package(self, pkg_dir):
+        """Builds the package when the local system is FreeBSD."""
         # If we are running on FreeBSD, make package. Otherwise display warning that package was not compiled
         if platform.system() == "FreeBSD":
-            s = subprocess.call(["/usr/bin/make", "package", "-C", pkg_dir, "DISABLE_VULNERABILITIES=yes"])
+            subprocess.call(["/usr/bin/make", "package", "-C", pkg_dir, "DISABLE_VULNERABILITIES=yes"])
         else:
             print("WARNING: System is not FreeBSD. Generated Makefile and pkg-plist but did not attempt to build pkg.")
 
     def build_on_remote_host(self):
-        # Automate the process to pull, build and retrieve the package on a remote host
+        """Runs the build on a remote host using SSH."""
+        # Automate the process to pull, install dependencies, build and retrieve the package on a remote host
         build_cmds = [
             "mkdir -p ~/build/",
-            "rm -rf ~/build/pfsense-saml2-auth",
-            "git clone https://github.com/jaredhendrickson13/pfsense-saml2-auth.git ~/build/pfsense-saml2-auth/",
-            "git -C ~/build/pfsense-saml2-auth checkout " + self.args.branch,
-            "python3 ~/build/pfsense-saml2-auth/tools/make_package.py --tag {t}".format(t=self.args.tag)
+            f"rm -rf ~/build/{REPO_NAME}",
+            f"git clone https://github.com/jaredhendrickson13/{REPO_NAME}.git ~/build/{REPO_NAME}/",
+            f"git -C ~/build/{REPO_NAME} checkout " + self.args.branch,
+            f"composer install --working-dir ~/build/{REPO_NAME}",
+            f"rm -rf ~/build/{REPO_NAME}/vendor/composer && rm ~/build/{REPO_NAME}/vendor/autoload.php",
+            f"cp -r ~/build/{REPO_NAME}/vendor/* ~/build/{REPO_NAME}/{PKG_NAME}/files/etc/inc/",
+            f"python3 ~/build/{REPO_NAME}/tools/make_package.py --tag {self.args.tag}"
         ]
 
-        # Join our build commands into a single command to run via SSH
-        self.run_ssh_cmd(" && ".join(build_cmds))
+        # Run each command and exit on bad status if failure
+        for cmd in build_cmds:
+            if self.run_ssh_cmd(cmd) != 0:
+                print(f"Command '{cmd}' failed.")
+                sys.exit(1)
 
         # Retrieve the built package
-        if self.args.freebsd == 11:
-            src = "{u}@{h}:~/build/pfsense-saml2-auth/pfSense-pkg-saml2-auth/pfSense-pkg-saml2-auth-{v}{r}.txz"
-            src = src.format(
-                u=self.args.username,
-                h=self.args.host,
-                v=self.port_version,
-                r="_" + self.port_revision if self.port_revision != "0" else ""
-            )
-            self.run_scp_cmd(src, ".")
-        else:
-            src = "{u}@{h}:~/build/pfsense-saml2-auth/pfSense-pkg-saml2-auth/work/pkg/pfSense-pkg-saml2-auth-{v}{r}.txz"
-            src = src.format(
-                u=self.args.username,
-                h=self.args.host,
-                v=self.port_version,
-                r="_" + self.port_revision if self.port_revision != "0" else ""
-            )
-            self.run_scp_cmd(src, ".")
+        u=self.args.username
+        h=self.args.host
+        v=self.port_version
+        r="_" + self.port_revision if self.port_revision != "0" else ""
+        src = f"{u}@{h}:~/build/{REPO_NAME}/{PKG_NAME}/work/pkg/{PKG_NAME}-{v}{r}.pkg"
+
+        self.run_scp_cmd(src, ".")
 
     def __start_argparse__(self):
         # Custom port type for argparse
@@ -138,25 +148,18 @@ class MakePackage:
             # Return value if valid, otherwise throw error
             if valid:
                 return value_string
-            else:
-                raise argparse.ArgumentTypeError("%s is not a semantic version tag" % value_string)
+
+            raise argparse.ArgumentTypeError(f"{value_string} is not a semantic version tag")
 
         parser = argparse.ArgumentParser(
-            description="Build the pfSense SAML2 package on FreeBSD"
+            description=f"Build the {PKG_NAME} on FreeBSD"
         )
         parser.add_argument(
             '--host', '-i',
             dest="host",
             type=str,
-            required=True if "--remote" in sys.argv or "-r" in sys.argv else False,
+            required=bool("--remote" in sys.argv or "-r" in sys.argv),
             help="The host to connect to when using --build mode"
-        )
-        parser.add_argument(
-            '--freebsd', '-f',
-            dest="freebsd",
-            type=int,
-            default=12,
-            help="The version of FreeBSD running on the remote host"
         )
         parser.add_argument(
             '--branch', '-b',
@@ -178,13 +181,6 @@ class MakePackage:
             type=tag,
             required=True,
             help="The version tag to use when building."
-        )
-        parser.add_argument(
-            '--remote', '-r',
-            dest="build",
-            action="store_true",
-            required=False,
-            help='Enable remote build mode'
         )
         self.args = parser.parse_args()
 
